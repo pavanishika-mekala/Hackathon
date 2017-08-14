e5df678c0849c09af307680de5d03c9c0407cdf3
Vaishali Kammeta <vaishali.kammeta@kony.com>
2017-08-14 17:32:51 +0530

    BBE - 13 Review comments for the actions created for this ticket, deletting the unused actions

--- MyApprovals/studioactions/mobile/frmlogininitAction.js
+++ MyApprovals/studioactions/mobile/frmlogininitAction.js
@@ -4,5 +4,4 @@ function frmlogininitAction(eventobject) {
 
 function AS_Form_28b52343d3b04392860dbdeb62b6d341(eventobject) {
     kony.apps.coe.ess.frmLogin.Init();
-    kony.apps.coe.ess.locale.getLocaleInformation();
 }
