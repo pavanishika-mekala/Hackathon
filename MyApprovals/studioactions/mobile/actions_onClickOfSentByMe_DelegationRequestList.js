8cee8ea101dbc11b5ce629d35ff31352ed51b03c
Vaishali Kammeta <vaishali.kammeta@kony.com>
2017-08-30 20:03:52 +0530

    Adding actions for delegation screen sent By me and Received buttons and added action for i18n locale changes in startup screen

---
+++ MyApprovals/studioactions/mobile/actions_onClickOfSentByMe_DelegationRequestList.js
@@ -0,0 +1,7 @@
+function actions_onClickOfSentByMe_DelegationRequestList(eventobject) {
+    return AS_Button_g998e61ac5f8446dac7cd97cd563667b(eventobject);
+}
+
+function AS_Button_g998e61ac5f8446dac7cd97cd563667b(eventobject) {
+    kony.apps.coe.ess.Approvals.DelegationRequestList.Actions.getInstance().onClickOfSentByMe();
+}
