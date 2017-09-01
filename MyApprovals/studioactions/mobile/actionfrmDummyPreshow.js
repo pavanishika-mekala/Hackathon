37b7c5564d3724f0189b7bb8376480a4d99f39df
Soumya.Sriram <soumya.sriram@kony.com>
2017-09-01 16:51:11 +0530

    language change after doing relauch of the application

--- MyApprovals/studioactions/mobile/actionfrmDummyPreshow.js
+++ MyApprovals/studioactions/mobile/actionfrmDummyPreshow.js
@@ -5,4 +5,5 @@ function actionfrmDummyPreshow(eventobject) {
 function AS_Form_0602472c253d49cd98b6f61933fff06a(eventobject) {
     kony.modules.loadFunctionalModule("librarymodules");
     kony.modules.loadFunctionalModule("appjsmodules");
+    kony.apps.coe.ess.locale.getLocaleInformation();
 }
