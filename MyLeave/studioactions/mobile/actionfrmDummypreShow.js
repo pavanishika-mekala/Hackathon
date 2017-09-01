262754933eae82935e6d3dca050fc2742bc43cc7
Soumya.Sriram <soumya.sriram@kony.com>
2017-09-01 17:12:24 +0530

    language change relaunch issue for my leave app

--- MyLeave/studioactions/mobile/actionfrmDummypreShow.js
+++ MyLeave/studioactions/mobile/actionfrmDummypreShow.js
@@ -5,4 +5,5 @@ function actionfrmDummypreShow(eventobject) {
 function AS_Form_1665b560b5cb4067bbf66be9c9605104(eventobject) {
     kony.modules.loadFunctionalModule("librarymodules");
     kony.modules.loadFunctionalModule("appjsmodules");
+    kony.apps.coe.ess.locale.getLocaleInformation();
 }
