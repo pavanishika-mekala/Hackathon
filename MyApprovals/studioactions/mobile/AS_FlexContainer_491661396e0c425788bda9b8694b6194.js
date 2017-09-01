a11d7b5352f12c7f93e2bf99a49f4b2e5d9e2881
Soumya.Sriram <soumya.sriram@kony.com>
2017-09-01 16:11:45 +0530

    setting to back button issue

--- MyApprovals/studioactions/mobile/AS_FlexContainer_491661396e0c425788bda9b8694b6194.js
+++ MyApprovals/studioactions/mobile/AS_FlexContainer_491661396e0c425788bda9b8694b6194.js
@@ -1,3 +1,6 @@
 function AS_FlexContainer_491661396e0c425788bda9b8694b6194(eventobject) {
-    kony.application.getPreviousForm().show();
+    var previousForm = kony.application.getPreviousForm();
+    var formController = kony.sdk.mvvm.KonyApplicationContext.getAppInstance().getFormController(previousForm.id);
+    previousForm.destroy();
+    formController.loadDataAndShowForm();
 }
