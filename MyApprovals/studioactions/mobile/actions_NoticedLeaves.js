a8a67f34de0a004618b7c7888586cdaf73260941
Vaishali Kammeta <vaishali.kammeta@kony.com>
2017-09-05 16:24:15 +0530

    BBE 91 new button added in Approval home segment and action same as Approved

---
+++ MyApprovals/studioactions/mobile/actions_NoticedLeaves.js
@@ -0,0 +1,7 @@
+function actions_NoticedLeaves(eventobject, context) {
+    return AS_Button_ec36453068e24f8badd25d98997d9972(eventobject, context);
+}
+
+function AS_Button_ec36453068e24f8badd25d98997d9972(eventobject, context) {
+    kony.apps.coe.ess.Approvals.ApprovalsHome.approveRequest(context);
+}
