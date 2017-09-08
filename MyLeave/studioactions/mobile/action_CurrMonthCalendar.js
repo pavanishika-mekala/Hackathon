1d11b44eca4827a6b8a79060c35a4dda07776c03
Vaishali Kammeta <vaishali.kammeta@kony.com>
2017-09-08 19:38:51 +0530

    BBE 54 Added Go to today button and action added to it to get the current month calendar i18n key added for TODAY text

---
+++ MyLeave/studioactions/mobile/action_CurrMonthCalendar.js
@@ -0,0 +1,7 @@
+function action_CurrMonthCalendar(eventobject) {
+    return AS_FlexContainer_c2c3ece0cf354784b075517ce0eb8800(eventobject);
+}
+
+function AS_FlexContainer_c2c3ece0cf354784b075517ce0eb8800(eventobject) {
+    kony.apps.coe.ess.myLeave.TeamViewUI.prototype.addDynamicHorzCalendar();
+}
