0a0e78264a7cfa159abce3309754a1b9c8cc436f
Soumya.Sriram <soumya.sriram@kony.com>
2017-09-11 19:35:16 +0530

    done with review comments

--- MyApprovals/studioactions/mobile/AS_TextField_c391cd8e4799426b83b09553dc840ec1.js
+++ MyApprovals/studioactions/mobile/AS_TextField_c391cd8e4799426b83b09553dc840ec1.js
@@ -2,5 +2,4 @@ function AS_TextField_c391cd8e4799426b83b09553dc840ec1(eventobject, changedtext)
     var searchText = frmSelect.txtSearch.text;
     var filterobj = new kony.apps.coe.ess.Approvals.frmSelectBackendLogic();
     filterobj.searchDataInPeople(searchText);
-    //kony.apps.coe.ess.Approvals.frmSelectBackendLogic.getPeopleInstance().searchDataInPeople(searchText);
 }
--- MyApprovals/studioactions/mobile/AS_TextField_fed40fe607fe4586a625c7fce06f2c47.js
+++ MyApprovals/studioactions/mobile/AS_TextField_fed40fe607fe4586a625c7fce06f2c47.js
@@ -2,5 +2,4 @@ function AS_TextField_fed40fe607fe4586a625c7fce06f2c47(eventobject, changedtext)
     var searchText = frmSelect.txtSearch.text;
     var filterobj = new kony.apps.coe.ess.Approvals.frmSelectBackendLogic();
     filterobj.searchDataInPeople(searchText);
-    //kony.apps.coe.ess.Approvals.frmSelectBackendLogic.getPeopleInstance().searchDataInPeople(searchText);
 }
