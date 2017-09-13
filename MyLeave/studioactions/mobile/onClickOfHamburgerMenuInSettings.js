12d691bbbaf8b19b717a44051a50dcaad25c1937
Soumya.Sriram <soumya.sriram@kony.com>
2017-09-13 13:01:40 +0530

    Defect:language change in MyLeave when starting from the search tab leads to the showing of dummy results in the search screen ,TeamView

--- MyLeave/studioactions/mobile/onClickOfHamburgerMenuInSettings.js
+++ MyLeave/studioactions/mobile/onClickOfHamburgerMenuInSettings.js
@@ -7,5 +7,18 @@ function AS_FlexContainer_d4322bf857dc4fc78fc29eb1c4fee9a4(eventobject) {
     var previousForm = kony.application.getPreviousForm();
     var formController = kony.sdk.mvvm.KonyApplicationContext.getAppInstance().getFormController(previousForm.id);
     previousForm.destroy();
-    formController.loadDataAndShowForm();
+    if (previousForm.id == "frmSearchLog") {
+        kony.apps.coe.ess.MyLeave.Footer.navigateFooter(3)
+    } else if (previousForm.id == "frmTeamView") {
+        (new kony.apps.coe.ess.myLeave.TeamViewUI()).toggleSkin(frmLeaveHome, true);
+        var FormLoadObj = new kony.sdk.mvvm.NavigationObject();
+        var qp = {
+            "employeeId": kony.apps.coe.ess.globalVariables.employeeId
+        };
+        FormLoadObj.setQueryParams("segTeamView", qp);
+        var formController = kony.sdk.mvvm.KonyApplicationContext.getAppInstance().getFormController("frmTeamView");
+        formController.loadDataAndShowForm(FormLoadObj)
+    } else {
+        formController.loadDataAndShowForm();
+    }
 }
