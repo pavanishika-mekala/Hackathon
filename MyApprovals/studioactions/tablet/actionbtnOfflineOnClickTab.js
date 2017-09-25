b7055afb3ad544e8e30928ce60824f5f1986adc6
Soumya.Sriram <soumya.sriram@kony.com>
2017-09-25 20:05:01 +0530

    BBE-97 ffi toast message

---
+++ MyApprovals/studioactions/tablet/actionbtnOfflineOnClickTab.js
@@ -0,0 +1,7 @@
+function actionbtnOfflineOnClickTab(eventobject) {
+    return AS_Button_c0d7cbd7174c4770b43bb479c3193618(eventobject);
+}
+
+function AS_Button_c0d7cbd7174c4770b43bb479c3193618(eventobject) {
+    (new kony.apps.coe.ess.Hamburger()).btnOfflineOnClick();
+}
