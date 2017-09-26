8a577f186ed79025fd9f43a324ddde85ef8a0230
Vaishali Kammeta <vaishali.kammeta@kony.com>
2017-09-26 19:16:08 +0530

    BBE 139 Font increased for Tablet screens MyLeave

--- MyLeave/studioactions/tablet/AS_Segment_ef7ce0c5e7d249aab9fcab1dda0575ff.js
+++ MyLeave/studioactions/tablet/AS_Segment_ef7ce0c5e7d249aab9fcab1dda0575ff.js
@@ -1,4 +1,3 @@
 function AS_Segment_ef7ce0c5e7d249aab9fcab1dda0575ff(eventobject, sectionNumber, rowNumber) {
-    var obj = new kony.apps.coe.myLeave.search();
-    obj.onDone2();
+    (new kony.apps.coe.myLeave.searchStatus()).rowClick();
 }
