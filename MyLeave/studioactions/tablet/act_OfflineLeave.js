35e5fa54440387627d9e145ef257f2a44f073449
Vaishali Kammeta <vaishali.kammeta@kony.com>
2017-09-26 12:10:39 +0530

    Actoin missed for btnOffline in ApplyLeave

---
+++ MyLeave/studioactions/tablet/act_OfflineLeave.js
@@ -0,0 +1,7 @@
+function act_OfflineLeave(eventobject) {
+    return AS_Button_f031c469a4894ef88746b75a0ef91081(eventobject);
+}
+
+function AS_Button_f031c469a4894ef88746b75a0ef91081(eventobject) {
+    kony.application.getCurrentForm().flxOfflineAlert.setVisibility(false);
+}
