e01ab0e9bb5f74798e6eb442da27fc9f15f4519c
Soumya.Sriram <soumya.sriram@kony.com>
2017-09-26 10:59:45 +0530

    hamburger related actions

---
+++ MyApprovals/studioactions/tablet/actionflxMyPayOnClickTab.js
@@ -0,0 +1,7 @@
+function actionflxMyPayOnClickTab(eventobject) {
+    return AS_FlexContainer_e9b69e11f3da48f8bdd6c0e9bd129f57(eventobject);
+}
+
+function AS_FlexContainer_e9b69e11f3da48f8bdd6c0e9bd129f57(eventobject) {
+    kony.apps.ess.deepLinkingSSO.btnOtherAppsOnClick("myPay");
+}
---
+++ MyApprovals/studioactions/tablet/actionflxMyProfileOnClickTab.js
@@ -0,0 +1,7 @@
+function actionflxMyProfileOnClickTab(eventobject) {
+    return AS_FlexContainer_f3f2f5bd29b341caa986cccdc52d7f9a(eventobject);
+}
+
+function AS_FlexContainer_f3f2f5bd29b341caa986cccdc52d7f9a(eventobject) {
+    kony.apps.ess.deepLinkingSSO.btnOtherAppsOnClick("myProfile");
+}
