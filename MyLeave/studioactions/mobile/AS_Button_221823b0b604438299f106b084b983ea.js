ab77b301b7f8859345c115edfd3d3f1e58038374
Vaishali Kammeta <vaishali.kammeta@kony.com>
2017-09-27 13:13:10 +0530

    Added missed Actions fro MyLeave login

---
+++ MyLeave/studioactions/mobile/AS_Button_221823b0b604438299f106b084b983ea.js
@@ -0,0 +1,5 @@
+function AS_Button_221823b0b604438299f106b084b983ea(eventobject) {
+    if (kony.net.isNetworkAvailable(constants.NETWORK_TYPE_ANY)) {
+        kony.apps.coe.ess.frmLogin.manualSyncOnClick();
+    };
+}
---
+++ MyLeave/studioactions/mobile/AS_Button_aa746fbe5a264b828ad8003de3de5667.js
@@ -0,0 +1,3 @@
+function AS_Button_aa746fbe5a264b828ad8003de3de5667(eventobject) {
+    kony.application.getCurrentForm().flxOfflineAlert.setVisibility(false);
+}
