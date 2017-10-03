f11c1d015c516f9aa9f48a0168d126002a021c61
Vaishali Kammeta <vaishali.kammeta@kony.com>
2017-10-03 19:53:28 +0530

    Action added for flxAllHoliday

--- MyLeave/studioactions/tablet/AS_FlexContainer_dbe8cb9ebe414cca92edc60b7688e621.js
+++ MyLeave/studioactions/tablet/AS_FlexContainer_dbe8cb9ebe414cca92edc60b7688e621.js
@@ -1,3 +1,7 @@
 function AS_FlexContainer_dbe8cb9ebe414cca92edc60b7688e621(eventobject) {
+    return AS_FlexContainer_abdecf732c6b4d9a8d0c3b9adf4dbce6(eventobject);
+}
+
+function AS_FlexContainer_abdecf732c6b4d9a8d0c3b9adf4dbce6(eventobject) {
     frmHolidays.show();
 }
