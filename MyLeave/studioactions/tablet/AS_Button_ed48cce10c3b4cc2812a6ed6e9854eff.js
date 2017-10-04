234955ce306170e74e954593d288cc10ab857c2c
Vaishali Kammeta <vaishali.kammeta@kony.com>
2017-10-04 13:24:27 +0530

    MyLeave Tab support biweekly button onClick action added and skins forked for mobile is removed

--- MyLeave/studioactions/tablet/AS_Button_ed48cce10c3b4cc2812a6ed6e9854eff.js
+++ MyLeave/studioactions/tablet/AS_Button_ed48cce10c3b4cc2812a6ed6e9854eff.js
@@ -1,1 +1,3 @@
-function AS_Button_ed48cce10c3b4cc2812a6ed6e9854eff(eventobject) {}
+function AS_Button_ed48cce10c3b4cc2812a6ed6e9854eff(eventobject) {
+    kony.apps.coe.ess.myLeave.applyLeave.RecurringLeave.onClickOfBiweekly();
+}
