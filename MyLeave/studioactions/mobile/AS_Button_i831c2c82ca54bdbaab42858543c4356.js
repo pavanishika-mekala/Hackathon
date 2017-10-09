e74885b06b1e5e943c3c1db8e656f85f44c6230b
Vaishali Kammeta <vaishali.kammeta@kony.com>
2017-10-09 10:30:04 +0530

    BBE 148 and BBE 149 MyLeave MyApprovals Change language button implementation

---
+++ MyLeave/studioactions/mobile/AS_Button_i831c2c82ca54bdbaab42858543c4356.js
@@ -0,0 +1,3 @@
+function AS_Button_i831c2c82ca54bdbaab42858543c4356(eventobject) {
+    kony.apps.coe.ess.myLeave.MyLeaveHomeUI.showLeaveHome();
+}
---
+++ MyLeave/studioactions/tablet/AS_Button_je5d89ca941746668bb38ddddc48550e.js
@@ -0,0 +1,3 @@
+function AS_Button_je5d89ca941746668bb38ddddc48550e(eventobject) {
+    kony.apps.coe.ess.myLeave.MyLeaveHomeUI.showLeaveHome();
+}
