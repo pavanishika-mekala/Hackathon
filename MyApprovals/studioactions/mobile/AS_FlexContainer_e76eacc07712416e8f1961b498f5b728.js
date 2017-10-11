2179776c68482a014786cf5dab8578fb804f3a90
Vaishali Kammeta <vaishali.kammeta@kony.com>
2017-10-11 15:55:50 +0530

    BBE 149 OnClick actiona added for btn Nederlands

---
+++ MyApprovals/studioactions/mobile/AS_FlexContainer_e76eacc07712416e8f1961b498f5b728.js
@@ -0,0 +1,3 @@
+function AS_FlexContainer_e76eacc07712416e8f1961b498f5b728(eventobject) {
+    kony.apps.coe.ess.settings.getSettingsObject().languageSelection("unselectit.png", "unselectit.png", "selectit.png", "nl_BE");
+}
