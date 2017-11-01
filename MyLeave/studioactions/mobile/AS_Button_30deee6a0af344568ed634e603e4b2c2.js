1003237a03247acee1e2d22eeedbcf466e297dc7
Vaishali Kammeta <vaishali.kammeta@kony.com>
2017-11-01 15:12:53 +0530

    BBE - 185 When there is no quota to apply for leave , showing a toast message to user

--- MyLeave/studioactions/mobile/AS_Button_30deee6a0af344568ed634e603e4b2c2.js
+++ MyLeave/studioactions/mobile/AS_Button_30deee6a0af344568ed634e603e4b2c2.js
@@ -1,3 +1,8 @@
 function AS_Button_30deee6a0af344568ed634e603e4b2c2(eventobject) {
-    kony.apps.coe.ess.myLeave.applyLeave.onClickOfApplySubmit();
+    if (frmApplyLeave.flxLeaveBalanceDetails.isVisible) {
+        kony.apps.coe.ess.myLeave.applyLeave.onClickOfApplySubmit();
+    } else {
+        toastMsg.showToastMsg("Requested leaves are insufficient", 2000);
+    }
+    //kony.apps.coe.ess.myLeave.applyLeave.onClickOfApplySubmit();
 }
--- MyLeave/studioactions/tablet/AS_Button_a5135a69ea244d5fa262812c7c9e83a2.js
+++ MyLeave/studioactions/tablet/AS_Button_a5135a69ea244d5fa262812c7c9e83a2.js
@@ -1,3 +1,8 @@
 function AS_Button_a5135a69ea244d5fa262812c7c9e83a2(eventobject) {
-    kony.apps.coe.ess.myLeave.applyLeave.onClickOfApplySubmit();
+    if (frmApplyLeave.flxLeaveBalanceDetails.isVisible) {
+        kony.apps.coe.ess.myLeave.applyLeave.onClickOfApplySubmit();
+    } else {
+        toastMsg.showToastMsg("Requested leaves are insufficient", 2000);
+    }
+    //kony.apps.coe.ess.myLeave.applyLeave.onClickOfApplySubmit();
 }
