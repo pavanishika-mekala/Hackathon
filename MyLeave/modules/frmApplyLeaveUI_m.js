c444951ac165dd20fdc43b130ce919c2e1045aad
Vaishali Kammeta <vaishali.kammeta@kony.com>
2017-11-14 19:47:53 +0530

    Available coount mapping to balance column in employee_leave_type table

--- MyLeave/modules/frmApplyLeaveUI_m.js
+++ MyLeave/modules/frmApplyLeaveUI_m.js
@@ -497,7 +497,7 @@ kony.apps.coe.ess.myLeave.applyLeave.LeaveType = {
     var sqlquery = "select * from employee_leave_type where leave_type_id = '" + leave_type_id + "'";
     kony.sync.single_select_execute(kony.sync.getDBName(), sqlquery, null, function(data) {
       if (data.length > 0 && data !== undefined && data[0].balance !== undefined) {
-        frmApplyLeave.lblLeaveBalanceCount.text = (data[0].balance - data[0].availed).toFixed(0);
+        frmApplyLeave.lblLeaveBalanceCount.text = (data[0].balance).toFixed(1); // - data[0].availed
         //remove frmApplyLeave.lblLeaveTypeBalance.text = data[0].balance - data[0].availed;
         //frmApplyLeave.lblLeaveTypeBalance.text = "Available " + frmApplyLeave[eventobject.id].text + " leave";
         //remove frmApplyLeave.lblLeaveTypeBalance.text = "Available " + id+ " leave";
