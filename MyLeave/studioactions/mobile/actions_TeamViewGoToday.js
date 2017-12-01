7d6c5b6162e02e73be242d98eacf851501820bb7
Soumya.Sriram <soumya.sriram@kony.com>
2017-12-01 19:20:37 +0530

    BBE-303  ML_Team view_Swipe is not working properly for the team view leaves

--- MyLeave/studioactions/mobile/actions_TeamViewGoToday.js
+++ MyLeave/studioactions/mobile/actions_TeamViewGoToday.js
@@ -6,5 +6,12 @@ function AS_FlexContainer_e1ec7c04fd554eb791b36d71113383d9(eventobject) {
     if (kony.os.deviceInfo().name === "iPhone") {
         frmTeamView.flxDayBar.removeAll();
     }
-    kony.apps.coe.ess.myLeave.TeamViewUI.prototype.addDynamicHorzCalendar();
+    //kony.apps.coe.ess.myLeave.TeamViewUI.prototype.addDynamicHorzCalendar();
+    var FormLoadObj = new kony.sdk.mvvm.NavigationObject();
+    var qp = {
+        "employeeId": kony.apps.coe.ess.globalVariables.employeeId
+    };
+    FormLoadObj.setQueryParams("segTeamView", qp);
+    var formController = kony.sdk.mvvm.KonyApplicationContext.getAppInstance().getFormController("frmTeamView");
+    formController.loadDataAndShowForm(FormLoadObj);
 }
