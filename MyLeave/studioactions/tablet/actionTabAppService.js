4af998534e7614ab545832df4abb042bf55a1270
Soumya.Sriram <soumya.sriram@kony.com>
2017-12-13 19:03:33 +0530

    Committing Harsha changes(BBE-279:Committing MyLeave changes).

--- MyLeave/studioactions/tablet/actionTabAppService.js
+++ MyLeave/studioactions/tablet/actionTabAppService.js
@@ -3,5 +3,6 @@ function actionTabAppService(eventobject) {
 }
 
 function AS_AppEvents_ca2a337bf95f4ca4a604b172903f99c4(eventobject) {
+    kony.apps.coe.ess.KMS.setPushNotificationCallbacks();
     return kony.apps.ess.deepLinkingSSO.appServiceCallback(eventobject);
 }
