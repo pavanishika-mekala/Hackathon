2dfdf4d3d1d51a61965f5d9179d45980f1eeeca5
Pavanishika Mekala <KIT798@KITHYD.COM>
2017-12-21 15:03:35 +0530

    colorpalette functionality added. animating palette to 360 degrees

--- controllers/mobile/frmHomeController/frmHomeController.js
+++ controllers/mobile/frmHomeController/frmHomeController.js
@@ -8,16 +8,16 @@ define({
       this._isInit=true;
       var controller = this;
       var PalatteEntry = require("com/Hackathon/ColorPalette/ColorPaletteEntry");
-      controller.view.ColorPalette.add(new PalatteEntry("Entry1","",function () {alert("click 1");}));
-      controller.view.ColorPalette.add(new PalatteEntry("Entry2","",function () {alert("click 1");}));
-      controller.view.ColorPalette.add(new PalatteEntry("Entry3","",function () {alert("click 1");}));
-      controller.view.ColorPalette.add(new PalatteEntry("Entry4","",function () {alert("click 1");}));
-      controller.view.ColorPalette.add(new PalatteEntry("Entry5","",function () {alert("click 1");}));
-      controller.view.ColorPalette.add(new PalatteEntry("Entry6","",function () {alert("click 1");}));
-      controller.view.ColorPalette.add(new PalatteEntry("Entry7","",function () {alert("click 1");}));
-      controller.view.ColorPalette.add(new PalatteEntry("Entry8","",function () {alert("click 1");}));
-      controller.view.ColorPalette.add(new PalatteEntry("Entry9","",function () {alert("click 1");}));
-      controller.view.ColorPalette.add(new PalatteEntry("Entry10","",function () {alert("click 1");}));
+      controller.view.ColorPalette.add(new PalatteEntry("Entry1","00B898",function () {alert("click 1");}));
+      controller.view.ColorPalette.add(new PalatteEntry("Entry2","3aa339",function () {alert("click 1");}));
+      controller.view.ColorPalette.add(new PalatteEntry("Entry3","633EA5",function () {alert("click 1");}));
+      controller.view.ColorPalette.add(new PalatteEntry("Entry4","4286f4",function () {alert("click 1");}));
+      controller.view.ColorPalette.add(new PalatteEntry("Entry5","56f441",function () {alert("click 1");}));
+      controller.view.ColorPalette.add(new PalatteEntry("Entry6","f44141",function () {alert("click 1");}));
+      controller.view.ColorPalette.add(new PalatteEntry("Entry7","4038a2",function () {alert("click 1");}));
+      controller.view.ColorPalette.add(new PalatteEntry("Entry8","f441df",function () {alert("click 1");}));
+      controller.view.ColorPalette.add(new PalatteEntry("Entry9","f1f441",function () {alert("click 1");}));
+      controller.view.ColorPalette.add(new PalatteEntry("Entry10","FFBC00",function () {alert("click 1");}));
     }
   },
 
--- userwidgets/com.Hackathon.ColorPalette/modules/require/ColorPaletteController.js
+++ userwidgets/com.Hackathon.ColorPalette/modules/require/ColorPaletteController.js
@@ -4,7 +4,40 @@ define(function() {
 
     paletteEntries:[],
 
+    _openByDefault : true,//default false
+    _touchStartDate : null,
+    _numberOfpaletteEntries : 0,
+    _numberOfCircles : 10,
+    _currentTopCircle : 0,
+    
+    topPositionUp : "63%",
+    _topPositionDown : "83%",
+    topCircleNumber :null,
+    centerX :0,
+    centerY :0,
+    touchStartAngle : 0,
+    touchMoveAngle : 0,
+    touchEndAngle : 0,
+    //placeholder for the deferred bubble grow/shrink animation for android
+    //the reason for this is that android does not handle multiple size change animations
+    //well due to UI thread problems
+    //this callback will be executed onTouchEnd and will grow the selected bubble and shrink all others
+    //the same is done in ios, but continously during onTouchMove
+    touchEndCallback : null,
+
+    diskDown : true,
+    dateflex1 : null,
+    textForIconVal :[],
+
+
+
     constructor: function(baseConfig, layoutConfig, pspConfig) {
+      
+      this._availableDegrees = (this._numberOfCircles-1)*36;
+      this._normalSliceDegrees = Math.round(this._availableDegrees/(this._numberOfCircles));//-1 as the first and last serve as stoppers and take only half the space
+    //available space - space which is taken by the inner ones /2 => space for the ones on the edges
+    this._firstSliceEnd = Math.round((this._availableDegrees-(this._normalSliceDegrees*(this._numberOfCircles-2)))/2);
+    this._lastSliceBeginning = (this._normalSliceDegrees*(this._numberOfCircles-2))+this._firstSliceEnd;
 
       this.view.preShow = this.generatePalette;
     },
@@ -19,27 +52,25 @@ define(function() {
 
     positionValues:{
 
-      palette0 :{X:"50%",Y:"14%",top:"-1%",bottom:"Default"},
-      palette1 :{X:"29%",Y:"22%",top:"10%",bottom:"Default"},
-      palette2 :{X:"18%",Y:"39%",top:"31.5%",bottom:"Default"},
-      palette3 :{X:"20%",Y:"61%",top:"Default",bottom:"28.5%"},
-      palette4 :{X:"33%",Y:"77%",top:"Default",bottom:"8%"},
-      palette5 :{X:"51%",Y:"82%",top:"Default",bottom:"0.5%"},
-      palette6 :{X:"70%",Y:"77%",top:"Default",bottom:"7%"},
-      palette7 :{X:"80%",Y:"61%",top:"Default",bottom:"28%"},
-      palette8 :{X:"81%",Y:"39%",top:"Default",bottom:"28%"},
-      palette9 :{X:"70%",Y:"22%",top:"Default",bottom:"28%"} 
+      palette0 :{X:"50%",Y:"14%"},//,top:"-1%",bottom:"Default"
+      palette1 :{X:"29%",Y:"22%"},//,top:"10%",bottom:"Default"
+      palette2 :{X:"18%",Y:"39%"},//,top:"31.5%",bottom:"Default"
+      palette3 :{X:"20%",Y:"61%"},//,top:"Default",bottom:"28.5%"
+      palette4 :{X:"33%",Y:"77%"},//,top:"Default",bottom:"8%"
+      palette5 :{X:"51%",Y:"82%"},//,top:"Default",bottom:"0.5%"
+      palette6 :{X:"70%",Y:"75%"},//,top:"Default",bottom:"7%"
+      palette7 :{X:"80%",Y:"61%"},//,top:"Default",bottom:"28%"
+      palette8 :{X:"81%",Y:"39%"},//,top:"Default",bottom:"28%"
+      palette9 :{X:"70%",Y:"22%"}//,top:"Default",bottom:"28%"
     },
 
     generatePalette : function(){
       for(var i=0;i<this.paletteEntries.length;i++){
-        //var palette = this.paletteEntries[i];
-        //this._numberOfpaletteEntries++;
+        var palette = this.paletteEntries[i];
+        //var skin = (palette.getColor()!=="") ? palette.getColor() : "flxBG00b898";
+        this._numberOfpaletteEntries++;
         var centerXVal=this.positionValues["palette"+i].X;
         var centerYVal=this.positionValues["palette"+i].Y;
-//         var topVal=positionValues["palette"+_numberOfpaletteEntries].top;
-//         var bottomVal=positionValues["palette"+_numberOfpaletteEntries].bottom;
-
         var flxList = new kony.ui.FlexContainer({
           "autogrowMode": kony.flex.AUTOGROW_NONE,
           "clipBounds": true,
@@ -51,14 +82,202 @@ define(function() {
           "layoutType": kony.flex.FREE_FORM,
           //"left": "100%",
           "skin": "flxBG00b898",
-          //         "top": topVal,
-          //         "bottom": bottomVal,
           "width": "50Dp",
           "zIndex": 2
         }, {}, {});
 
+        flxList.onTouchEnd = palette.getAction();
+        flxList.backgroundColor = palette.getColor();
         this.view.flxPalette.add(flxList);
       }
+      this.preShowDisplayDefaultDisk();
+    },
+
+
+    preShowDisplayDefaultDisk : function(){
+      kony.print("ColorPaletteController.js.preShowDisplayDefaultDisk");
+      if(this._openByDefault===true) {
+        this._turnWorldToCircle(0);
+        this.touchEndAngle=0;
+      }
+      else{
+        this.view.flxPaletteOuter.top = this._topPositionDown;
+      }
+      this.view.flxPalette.forceLayout();
+      this.view.flxPaletteOuter.onTouchStart = this._circleInit;
+      this.view.flxPaletteOuter.onTouchMove = this._moveCircle;
+      this.view.flxPaletteOuter.onTouchEnd = this._circleEnd;
+    },
+
+    _turnWorldToCircle : function(index) {
+      //Magical compensation for strange offset
+      var offset = 0;
+      if (index>0) {
+        offset = Math.max(Math.min(6-index,5),0);//make sure it's between 5 and 0
+      }
+      var deg = Math.round((this._availableDegrees/(this._numberOfCircles-1)*index+offset)*-1);
+      this._turnWorld(deg);
+    },
+
+    _turnWorld : function (deg,callback) {
+      var xfrm = kony.ui.makeAffineTransform();
+      xfrm.rotate(deg);
+      //Turning the main area
+      this.view.flxPalette.transform=xfrm;
+      this.view.flxPaletteInner.transform=xfrm;
+      this.view.flxOuterCircle.transform=xfrm;
+      //     _imgDiskMenuFG.transform=xfrm;
+      //     _imgDiskMenuBorder.transform=xfrm;
+      //     _imgDiskMenuBG.transform=xfrm;
+
+      var lblXfrm = kony.ui.makeAffineTransform();
+      var lblDeg = deg;
+      lblDeg = lblDeg*-1;
+      lblXfrm.rotate(lblDeg);
+
+      //Turning labels (containing the icons) as well as BG, border and light effects
+      //against the user-initiated rotation so that they appear as fixed
+      for(var i=0;i<this._numberOfpaletteEntries;i++){
+        this.view["flxList" + i].transform = lblXfrm;
+      }
+      if (callback!==undefined && callback!==null) {
+        callback();
+      }
+    },
+
+    _getTopCircleNumber :function (deg) {
+      if(deg <0) deg = deg *-1;
+      if (deg<=this._firstSliceEnd) {
+        return 0;
+      } else if (deg>=this._lastSliceBeginning) {
+        return (this._numberOfpaletteEntries-1);//normalise 8 to 7 of [0-7]
+      } else {
+        return Math.round(deg/this._normalSliceDegrees);
+      }
+    },
+
+    _circleInit:function (source,x,y){ 
+      this.centerX=this.view.flxPalette.frame.width/2;
+      this.centerY=this.view.flxPalette.frame.height/2;
+      //Instead of incrementing/decrementing the angle, calculate it from the position
+      //=> we use this as start angle to compensate for the start point of the move
+      var vecInX = x - this.centerX;// get the vector from center to input
+      var vecInY = y - this.centerY;
+      var rad = Math.atan2(vecInY, vecInX); // In radians
+      this.touchStartAngle = -(rad * 360 / Math.PI + this.touchEndAngle);
+
+      this.dateflex1 = new Date().getTime();
+    },
+
+    _moveCircle : function (source,x,y){ 
+      var vecInX = x - this.centerX;  // get the vector from center to input
+      var vecInY = y - this.centerY;
+
+      //Instead of incrementing/decrementing the angle, calculate it from the position
+      var rad = Math.atan2(vecInY, vecInX); // In radians
+      //Normally the calculation uses 180 not 360, but as we have only half the circle as 
+      //touch area we need to be able to do a full turn with only half the touch area
+      var deg = rad * 360 / Math.PI;
+      var multiplier = -1;
+
+      this.touchMoveAngle = (this.touchStartAngle+deg)*multiplier;
+      this.touchMoveAngle = Math.max(-360,Math.min(0,this.touchMoveAngle));
+      this._turnWorldToCircle(this.touchMoveAngle);
+      this._changeThePalette(this.touchMoveAngle);
+
+    },
+    
+    _changeThePalette : function (deg){
+    this.topCircleNumber=this._getTopCircleNumber(deg);
+    if (this.topCircleNumber!=this._currentTopCircle) {
+      this._currentTopCircle=this.topCircleNumber;
+      var bgColor = this.view["flxList"+(this.topCircleNumber)].backgroundColor;
+      this.view.flxHomePalette.backgroundColor = bgColor;
+      this.view.flxOuter1.backgroundColor = bgColor;
     }
+  },
+
+    _circleEnd : function(source,x,y){
+      this.touchEndAngle=this.touchMoveAngle;
+      //execute deferred bubble animation for android
+      //shows / hides palette if short tap
+      this._manageDiskAnim();
+      //Snaps world into optimal position for the current circle
+      this._turnWorldToCircle(this._currentTopCircle);
+    },
+
+    _manageDiskAnim : function(){
+      var dateflex2 = new Date().getTime();
+      var timeDiff = dateflex2 - this.dateflex1;
+      if(this._openByDefault===false && timeDiff < 150){
+        if(this.diskDown){
+          this.diskDown = false;
+          this._expandMenu();    
+        }else{
+          this.diskDown = true;
+          this._reduceMenu();
+        } 
+      }
+    },
+
+    _expandMenu : function (){
+      var animationObject =
+          {
+            "0":{"top":this.view.flxPaletteOuter.top,"stepConfig":{"timingFunction":kony.anim.LINEAR}},
+            "100":{"top":this._topPositionUp,"stepConfig":{"timingFunction":kony.anim.LINEAR}}
+          };
+
+      var animationConfig = 
+          {
+            "delay":0,
+            "duration": 0.3,
+            "iterationCount":1,
+            "direction":kony.anim.DIRECTION_NONE,
+            "fillMode":kony.anim.FILL_MODE_FORWARDS
+          };
+      this.view.flxPaletteOuter.animate(kony.ui.createAnimation(animationObject), animationConfig, {});
+      this._animateWorldToCircle(this._currentTopCircle,0.4);
+    }, 
+
+    _reduceMenu : function (){
+      this._animateWorldToCircle(0,0.0);
+      var animationObject =
+          {
+            "0":{"top":this.view.flxPaletteOuter.top},
+            "100":{"top":this._topPositionDown,"stepConfig":{"timingFunction":kony.anim.EASIN_OUT}}
+          };
+
+      var animationConfig = 
+          {
+            "delay":0.4,
+            "duration": 0.3,
+            "iterationCount":1,
+            "fillMode":kony.anim.FILL_MODE_FORWARDS
+          };
+      this.view.flxPaletteOuter.animate(kony.ui.createAnimation(animationObject), animationConfig, {});
+    },
+
+    _animateWorldToCircle : function (index,delay) {
+      var offset = 0;
+      if (index>0) {
+        offset = Math.max(Math.min(6-index,5),0);//make sure it's between 5 and 0
+      }
+      var deg = Math.round((this._availableDegrees/(this._numberOfCircles-1)*index+offset)*-1);
+      var xfrm = kony.ui.makeAffineTransform();
+      xfrm.rotate(deg);
+      var timingConfig = {"delay":delay,"iterationCount":1,"fillMode":kony.anim.FILL_MODE_FORWARDS,"duration":0.04};
+      var stepAnimation = kony.ui.createAnimation({
+        "100":{"transform":xfrm,"stepConfig":{"timingFunction":kony.anim.LINEAR}}
+      });   
+
+      this.view.flxPalette.animate(stepAnimation, timingConfig, {});
+      this.view.flxPaletteOuter.animate(stepAnimation, timingConfig, {});
+      this.view.flxPaletteInner.animate(stepAnimation, timingConfig, {});
+      this.view.flxOuterCircle.animate(stepAnimation, timingConfig, {});
+      //     _imgDiskMenuFG.animate(stepAnimation, timingConfig, {});
+      //     _imgDiskMenuBorder.animate(stepAnimation, timingConfig, {});
+      //     _imgDiskMenuBG.animate(stepAnimation, timingConfig, {});
+    },
+
   };
 });
