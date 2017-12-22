0a4234bf8c3bd010398379e914a454f560c9fab9
Pavanishika Mekala <KIT798@KITHYD.COM>
2017-12-22 12:13:24 +0530

    Calendar initial commit

---
+++ userwidgets/com.Hackathon.DefaultCalendar/modules/require/DefaultCalendarController.js
@@ -0,0 +1,155 @@
+define(function() {
+
+  return {
+
+    currentMonth : 0,
+    currentYear : 0,
+    _weekendColor : "",
+    _currentDayColor : "",
+
+    constructor: function(baseConfig, layoutConfig, pspConfig) {
+
+      this.view.preShow = this._resetCalendar;
+
+      this.view.flxCalendar.addGestureRecognizer(constants.GESTURE_TYPE_SWIPE, {fingers: 1},this._onGestureClosure);
+
+    },
+    //Logic for getters/setters of custom properties
+    initGettersSetters: function() {
+
+    },
+
+    _resetCalendar : function (){
+      var date = new Date();
+      this.currentMonth = date.getMonth();
+      this.currentYear = date.getFullYear();
+      var d = new Date(this.currentYear,this.currentMonth,1,0,0,0,0);
+      this._assignDates(d.getDay(),this.currentMonth,this.currentYear);
+    },
+
+    _assignDates : function (firstDay,currentMonth,currentYear){
+      kony.print("#### assignDates #### currentMonth - "+currentMonth+" - currentYear - "+currentYear);
+      for(var j = 1; j < 38; j++){
+        this.view["lbl"+j].text = "";
+        this.view["lbl"+j].setVisibility(false);
+      }
+      var day30Months = [3,5,8,10];
+      var endDate = 0;
+      if(currentMonth == 1){
+        if(this.isLeapYear(currentYear)){
+          endDate = 29;
+        }else{
+          endDate = 28;  
+        }
+      }else if(day30Months.indexOf(currentMonth) > -1){
+        endDate = 30;
+      }else{
+        endDate = 31;
+      }
+      for(var i = 1; i <= endDate; i++){
+        this.view["lbl"+(firstDay+i)].text = i.toFixed(0);
+        this.view["lbl"+(firstDay+i)].setVisibility(true);
+      }
+      //_formName.lblMonth.text = _getMonth(currentMonth)+" "+currentYear;
+    },
+
+    isLeapYear : function(year)
+    {
+      return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+    },
+
+    _nextMonth : function(){
+      if(this.currentMonth == 11){
+        this.currentMonth = 0;
+        this.currentYear = this.currentYear+1;
+      }else{
+        this.currentMonth = this.currentMonth+1;
+      }
+      var d = new Date(this.currentYear,this.currentMonth,1,0,0,0,0);
+      this._assignDates(d.getDay(),this.currentMonth,this.currentYear);
+      this.setWeekendColor(this._weekendColor);
+    },
+
+    _previousMonth : function(){
+      if(this.currentMonth === 0){
+        this.currentMonth = 11;
+        this.currentYear = this.currentYear-1;
+      }else{
+        this.currentMonth = this.currentMonth-1;
+      }
+      var d = new Date(this.currentYear,this.currentMonth,1,0,0,0,0);
+      this._assignDates(d.getDay(),this.currentMonth,this.currentYear);
+      this.setWeekendColor(this._weekendColor);
+    },
+
+    _getMonth : function (month){
+      if(month === 0){
+        return "January";
+      }else if(month == 1){
+        return "February";
+      }else if(month == 2){
+        return "March";
+      }else if(month == 3){
+        return "April";
+      }else if(month == 4){
+        return "May";
+      }else if(month == 5){
+        return "June";
+      }else if(month == 6){
+        return "July";
+      }else if(month == 7){
+        return "August";
+      }else if(month == 8){
+        return "September";
+      }else if(month == 9){
+        return "October";
+      }else if(month == 10){
+        return "November";
+      }else if(month == 11){
+        return "December";
+      }
+    },
+
+    setCalendarColor : function (skinColor){
+      this.view.flxCalendar.skin = skinColor;
+      //     _formName.flxBar1.skin = skinColor;
+      //     _formName.flxBar2.skin = skinColor;
+    },
+
+    setWeekendColor : function (skinColor){
+      this._weekendColor = skinColor;
+      this.view.lbl1.skin = skinColor;
+      for(var j = 7; j < 38;){
+        this.view["lbl"+j].skin = skinColor;
+        this.view["lbl"+(j+1)].skin = skinColor;
+        j += 7;
+      }
+      if(this._currentDayColor.length > 0){
+        this.setCurrentDateColor(this._currentDayColor);
+      }
+    },
+
+    setCurrentDateColor : function (skinColor){
+      this._currentDayColor = skinColor;
+      var d = new Date();
+      if(this.currentYear == d.getFullYear() && this.currentMonth == d.getMonth()){
+        for(var j = 1; j < 38; j++){
+          if(this.view["lbl"+j].text == d.getDate()){
+            this.view["lbl"+j].skin = skinColor;
+          }
+        }
+      }
+    },
+
+
+    _onGestureClosure:function (widgetRef, gestureInfo, context){
+      if(gestureInfo.swipeDirection == 3){
+        this._nextMonth();
+      }else if(gestureInfo.swipeDirection == 4){
+        this._previousMonth();
+      }
+    }
+
+
+  };
+});
