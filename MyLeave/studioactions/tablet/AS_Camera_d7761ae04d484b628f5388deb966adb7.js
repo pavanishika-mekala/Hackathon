75b11c70f858dddbc2c3ab76c045b3bb582c5735
Soumya Sriram <KIT540@KITHYD.COM>
2017-12-27 19:47:16 +0530

    on click of camera function name correction

--- MyLeave/studioactions/tablet/AS_Camera_d7761ae04d484b628f5388deb966adb7.js
+++ MyLeave/studioactions/tablet/AS_Camera_d7761ae04d484b628f5388deb966adb7.js
@@ -8,6 +8,6 @@ function AS_Camera_d7761ae04d484b628f5388deb966adb7(eventobject) {
         kony.apps.coe.ess.myLeave.applyLeave.Attachment.onClickOfCancel();
         toastMsg.showToastMsg(kony.i18n.getLocalizedString("i18n.ess.myLeave.common.imagesize"), 3000);
     } else {
-        kony.apps.coe.ess.myLeave.applyLeave.Attachment.onClickOfTakePicture.onClickOfTakePicture(base64Val, "");
+        kony.apps.coe.ess.myLeave.applyLeave.Attachment.onClickOfTakePicture(base64Val, "");
     }
 }
