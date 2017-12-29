e012a0e1a2fc28e02ad73d574366af602d1c4de4
KITHYD\KIT904 <mounika.jangala@kony.com>
2017-12-29 14:37:06 +0530

    frmTimeSheetCreateTab changes

---
+++ MyTime/studioactions/tablet/actn_PostShow_frmTimeSheetCreateTab.js
@@ -0,0 +1,9 @@
+function actn_PostShow_frmTimeSheetCreateTab(eventobject) {
+    return AS_Form_f850880c59934f24b71afcfe873ca7f3(eventobject);
+}
+
+function AS_Form_f850880c59934f24b71afcfe873ca7f3(eventobject) {
+    frmTimeSheetCreateTab.flxTimesheetDetails.top = "22.8%";
+    frmTimeSheetCreateTab.flxTimeLine.top = "31.6%";
+    frmTimeSheetCreateTab.forceLayout();
+}
